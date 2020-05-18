f092c660a9b4d8e4980a4a5ebaf9d49393da1133
Oscaraul <45414554+Oscaraul@users.noreply.github.com>
2020-05-18 15:00:49 -0500

    Ultimos movimientos

--- js/eventos.js
+++ js/eventos.js
@@ -5,7 +5,3 @@ function cambioFotoAstronauta() {
 function cambioFotoExtraterrestre() {
 	document.getElementById('extraterrestre').style="-webkit-filter: blur(5px); filter:blur(5px);"
 }
-
-function cambioFOtoPlaneta() {
-	document.getElementById('planetas').style="filter: hue-rotate(90deg;)"
-}
