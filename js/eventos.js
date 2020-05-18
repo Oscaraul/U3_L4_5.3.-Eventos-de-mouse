bd548a164909ffd8696e3add420d17d2ec30e4c0
Oscaraul <racsoluar_391@hotmail.com>
2020-05-18 00:25:27 -0500

    PracticaMouse

---
+++ js/eventos.js
@@ -0,0 +1,15 @@
+function cambioFotoAstronauta() {
+	document.getElementByid('astronauta').style="width:50%"
+}
+
+function cambioFotoExtraterrestre() {
+	document.getElementByid('extraterrestre').style="-webkit-filter: blur(5px); filter:blur(5px);"
+}
+
+function cambioFOtoPlaneta() {
+	document.getElementByid('planetas').style="filter: hue-rotate(90deg;)"
+}
+
+function cambioFotoNAve() {
+	document.getElementByid('nave_espacial').style="filter: sepia(100%); -webkit-filter: sepia(100%);"
+}
