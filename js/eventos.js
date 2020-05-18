157c5c8d7ed90291dbe37a17a0bbea650bc740d9
Oscaraul <45414554+Oscaraul@users.noreply.github.com>
2020-05-18 15:12:37 -0500

    Eliminé otra formula

--- js/eventos.js
+++ js/eventos.js
@@ -1,7 +1,3 @@
 function cambioFotoAstronauta() {
 	document.getElementById('astronauta').style="width:50%"
 }
-
-function cambioFotoExtraterrestre() {
-	document.getElementById('extraterrestre').style="-webkit-filter: blur(5px); filter:blur(5px);"
-}
